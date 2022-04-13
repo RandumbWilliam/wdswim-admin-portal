e9444f43f58ef980f962f1f0ccf7dd73688a704f
Cynthia Deng <cindy4213@gmail.com>
2022-04-13 00:29:25 -0400

    SAVING ADN COMMITING

--- server/index.js
+++ server/index.js
@@ -3,13 +3,10 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import usersRoutes from "./routes/users.js";
-<<<<<<< HEAD
 import discountsRoutes from "./routes/classSettings/discounts.js";
 import lessonTypeRoutes from "./routes/classSettings/lessonType.js";
-=======
 import classesRoutes from "./routes/classes.js";
 
->>>>>>> 528d4ef3f8fdc90ad9a42abe2b1fcb2106166c62
 
 dotenv.config();
 
@@ -20,12 +17,9 @@ app.use(cors());
 
 app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
-<<<<<<< HEAD
 app.use("/discounts", discountsRoutes);
 app.use("/lessonType", lessonTypeRoutes);
-=======
 app.use("/classes", classesRoutes);
->>>>>>> 528d4ef3f8fdc90ad9a42abe2b1fcb2106166c62
 
 app.use((err, req, res, next) => {
     console.log(err.stack);
